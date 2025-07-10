d5c84bc17df44fec4aed4dac77979c8c69251369
ShreyanshManiTripathi <shreyanshManiTripathi8@example.com>
2025-07-10 20:17:49 +0530

    changes

--- pages/makeAppointment.ts
+++ pages/makeAppointment.ts
@@ -13,20 +13,15 @@ export class makeAppointmentPage {
         const page = this.pom.getPage();
         await page.selectOption(selectors.appointment.facility, facility);
         await page.locator(selectors.appointment.comment).fill(comment);
-        // await page.locator(selectors.appointment.date).pressSequentially(date);
         await page.locator(selectors.appointment.hospital_readmission);
-         const checkbox = page.locator(selectors.appointment.hospital_readmission);
+        const checkbox = page.locator(selectors.appointment.hospital_readmission);
         const isChecked = await checkbox.isChecked();
         this.reAdmission = isChecked ? 'Yes' : 'No';
-        
         await page.locator(selectors.appointment.healthcareProgram).click();
         const healthCareProgram = await page.locator(selectors.appointment.healthcareProgram);
-        
         this.healthCareProgramValue = await healthCareProgram.inputValue();
         await page.locator(selectors.appointment.date).pressSequentially(date);
-
     }
-
 
     async submitAppointment() {
         const page = this.pom.getPage();
@@ -37,7 +32,7 @@ export class makeAppointmentPage {
         return this.reAdmission;
     }
     async getHealthCareProgramStatus() {
-       return this.healthCareProgramValue;
+        return this.healthCareProgramValue;
     }
 }
 
--- tests/cura.spec.ts
+++ tests/cura.spec.ts
@@ -3,32 +3,52 @@ import { PageObjectManager } from "../pages/poManager";
 import { loginPage } from "../pages/login";
 import data from "../testData/testdata.json";
 import { makeAppointmentPage } from '../pages/makeAppointment';
-import {selectors} from '../testData/locator.json';
+import { selectors } from '../testData/locator.json';
+test.describe('Login scenarios', () => {
 
-test('Cura login', async () => {
-    const login = new loginPage(new PageObjectManager());
-    await login.navigateToLogin(data.data[0].testData.URL);
-    await login.enterCredentials(data.data[0].testData.username, data.data[0].testData.password);
-    await login.submitLogin();
+    test('Valid login', async () => {
+
+        const login = new loginPage(new PageObjectManager());
+        await login.navigateToLogin(data.testData.data[0].fixAppointmentData.URL);
+        await login.enterCredentials(data.testData.data[0].fixAppointmentData.username, data.testData.data[0].fixAppointmentData.password);
+        await login.submitLogin();
+    })
+    test('Invalid username and valid password', async () => {
+        const pom = new PageObjectManager();
+        await pom.init();
+        const page = pom.getPage();
+        const login = new loginPage(new PageObjectManager());
+        await login.navigateToLogin(data.testData.data[0].fixAppointmentData.URL);
+        await login.enterCredentials(data.Incorrrectlogindata.selectors.login.username, data.Incorrrectlogindata.selectors.login.password);
+        await login.submitLogin();
+        await page.pause()
+        await page.waitForSelector(selectors.login.errorMessage, { timeout: 8000 });
+        const errorMessage = await page.locator(selectors.login.errorMessage).textContent();
+        expect(errorMessage).toContain(data.testData.data[0].fixAppointmentData.errorMessage)
+    });
+
 })
-
 test('Apointment Booking', async () => {
-   const pom = new PageObjectManager();
+    const pom = new PageObjectManager();
     await pom.init();
     const login = new loginPage(pom);
-    await login.navigateToLogin(data.data[0].testData.URL);
-    await login.enterCredentials(data.data[0].testData.username, data.data[0].testData.password);
+    await login.navigateToLogin(data.testData.data[0].fixAppointmentData.URL);
+    await login.enterCredentials(data.testData.data[0].fixAppointmentData.username, data.testData.data[0].fixAppointmentData.password);
     await login.submitLogin();
     const makeAppointment = new makeAppointmentPage(pom);
-    await makeAppointment.fillAppointmentDetails(data.data[0].testData.facility, data.data[0].testData.testComment, data.data[0].testData.appointmentDate);
+    await makeAppointment.fillAppointmentDetails(
+        data.testData.data[0].fixAppointmentData.facility,
+        data.testData.data[0].fixAppointmentData.testComment,
+        data.testData.data[0].fixAppointmentData.appointmentDate
+    );
     await pom.getPage().waitForTimeout(2000);
     await makeAppointment.submitAppointment();
     const page = pom.getPage();
-    await expect(page.locator(selectors.confirmation.header)).toHaveText(data.data[0].testData.appointmentConfirmationMessage)
-    await expect(page.locator(selectors.confirmation.facility)).toHaveText(data.data[0].testData.facility)
-    await expect(page.locator(selectors.confirmation.visit_date)).toHaveText(data.data[0].testData.appointmentDate)
+    await expect(page.locator(selectors.confirmation.header)).toHaveText(data.testData.data[0].fixAppointmentData.appointmentConfirmationMessage)
+    await expect(page.locator(selectors.confirmation.facility)).toHaveText(data.testData.data[0].fixAppointmentData.facility)
+    await expect(page.locator(selectors.confirmation.visit_date)).toHaveText(data.testData.data[0].fixAppointmentData.appointmentDate)
     await expect(page.locator(selectors.confirmation.program)).toHaveText(await makeAppointment.getHealthCareProgramStatus() ?? '')
     await expect(page.locator(selectors.confirmation.hospital_readmission)).toHaveText(await makeAppointment.getReAdmissionStatus() ?? '')
-    await expect(page.locator(selectors.confirmation.comment)).toHaveText(data.data[0].testData.testComment)
+    await expect(page.locator(selectors.confirmation.comment)).toHaveText(data.testData.data[0].fixAppointmentData.testComment)
     await pom.close();
 })
